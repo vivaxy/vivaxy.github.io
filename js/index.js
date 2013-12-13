c80cd7f7710ac881a0fbedd10ce409298f84b0d6
vivaxy <xyxuye2007@126.com>
2013-12-13 15:54:48 +0800

    use jquery to change style

--- js/index.js
+++ js/index.js
@@ -3,14 +3,14 @@ var color = ["#e74c3c","#e67e22","#f1c40f","#2ecc71","#3498db","#1abc9c","#9b59b
 var i = 0;
 var interval = 1000;
 $(document).ready(function(){
-	bg.style.width = window.innerWidth + "px";
-	bg.style.height = window.innerHeight + "px";
+	$("#bg").width(window.innerWidth + "px");
+	$("#bg").height(window.innerHeight + "px");
 	fade();
 });
 function fade(){
-	$("#bg").fadeTo(interval,1).fadeTo(interval,0,function(){changeColor();fade();})
-}
-function changeColor(){
-	bg.style.backgroundColor = color[i];
-	i = i+1<color.length && i+1 || 0;
+	$("#bg").fadeTo(interval,1).fadeTo(interval,0,function(){
+		$("#bg").css("background-color",color[i]);
+		i = i+1<color.length && i+1 || 0;
+		fade();
+	});
 }
