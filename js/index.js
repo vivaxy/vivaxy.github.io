2aeaf02be350255d6b889e404d84c6f0dca9eef4
vivaxy <xyxuye2007@126.com>
2013-12-13 10:47:56 +0800

    color show on index page

---
+++ js/index.js
@@ -0,0 +1,16 @@
+﻿var bg = document.getElementById("bg");
+var color = ["#e74c3c","#e67e22","#f1c40f","#2ecc71","#3498db","#1abc9c","#9b59b6"];
+var i = 0;
+var interval = 1000;
+$(document).ready(function(){
+	bg.style.width = window.innerWidth + "px";
+	bg.style.height = window.innerHeight + "px";
+	fade();
+});
+function fade(){
+	$("#bg").fadeTo(interval,1);
+	bg.style.backgroundColor = color[i];
+	i = i+1<color.length && i+1 || 0;
+	$("#bg").fadeTo(interval,0);
+	setTimeout(fade,interval*2);
+}
