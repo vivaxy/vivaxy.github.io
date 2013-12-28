1de60849f930b0e4991655286d798404fd775f29
vivaxy <xyxuye2007@126.com>
2013-12-28 22:11:19 +0800

    Update snake.js

--- p/snake/snake.js
+++ p/snake/snake.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById("canvas");
 canvas.style.position = "relative";
 canvas.style.display = "inline";
+canvas.style.top= "-340px";
+canvas.style.left= "560px;
 var ctx = canvas.getContext("2d");
 var body = { width : window.innerWidth-600, height : window.innerHeight,};
 if (body.width > body.height) {
