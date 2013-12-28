592b18c2c7b7fb50ff842aa0b33adf017e74d4f7
vivaxy <xyxuye2007@126.com>
2013-12-28 21:16:42 +0800

    Update tw.js

--- p/typewriter/tw.js
+++ p/typewriter/tw.js
@@ -1,7 +1,6 @@
 var div = document.getElementsByClassName("insetText")[0];
-div.style.width = window.innerWidth-40+"px";
-div.style.height = window.innerHeight-120+"px";
-document.getElementById("space").style.height = window.innerHeight-100 +"px"
+div.style.width = window.innerWidth+"px";
+div.style.height = window.innerHeight+"px";
 document.addEventListener("keypress", function(key){
 	var span = document.createElement("span");
 	span.innerText = String.fromCharCode(key.which);
