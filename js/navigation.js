a787f24058d6d417023cb2e0d7d0a7b106584b1e
vivaxy <xyxuye2007@126.com>
2013-12-28 15:55:17 +0800

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -33,24 +33,29 @@ $(document).ready(function(){
 		if (ns == 0){
 			$("#body").animate({left:300},300).addClass("box-shadow");
 			$("#navigation").animate({width:300,height:window.innerHeight},300);
+			$("#navigation #list").css({display:"block"}).animate({left:0},600);
 			$("#navigation .text").css({display:"block"});
-			$("#navigation #list").css({display:"block"});
 			ns = 1;
 		}else{
-			$("#navigation").animate({width:50,height:50},300);
-			$("#body").removeClass("box-shadow").animate({left:0},300);
+			$("#navigation #list").animate({left:-300},300,function(){
+				$(this).css({display:"none"});
+			});
+			$("#navigation").animate({width:50,height:50},600);
+			$("#body").removeClass("box-shadow").animate({left:0},600);
 			$("#navigation .text").css({display:"none"});
-			$("#navigation #list").css({display:"none"});
 			ns = 0;
 		}
 	});
 	$("#body").click(function(){
 		if (ns == 1){
-			$("#navigation").animate({width:50,height:50},300);
-			$("#body").removeClass("box-shadow").animate({left:0},300);
+			$("#navigation #list").animate({left:-300},300,function(){
+				$(this).css({display:"none"});
+			});
+			$("#navigation").animate({width:50,height:50},600);
+			$("#body").removeClass("box-shadow").animate({left:0},600);
 			$("#navigation .text").css({display:"none"});
-			$("#navigation #list").css({display:"none"});
 			ns = 0;
 		}
 	});
+	
 });
