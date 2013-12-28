b4fcaea855cb48baf1fe582d37ff59e7299f82dd
vivaxy <xyxuye2007@126.com>
2013-12-28 14:16:11 +0800

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -2,33 +2,55 @@ var xmlhttp=new XMLHttpRequest();
 xmlhttp.open("GET","/conf/navigation.xml",false);
 xmlhttp.send();
 var xmlDoc=xmlhttp.responseXML;
-var left = xmlDoc.getElementsByTagName("navigation")[0].getElementsByTagName("left")[0];
-document.write("<div class=\"navigation\"><ul>");
-var list = left.getElementsByTagName("list");
-for (var i=0;i<list.length;i++){
-	var head = list[i].getElementsByTagName("head")[0];
-	var n = head.getElementsByTagName("name")[0];
-	document.write("<li><a class=\"a\">");
-	document.write(n.childNodes[0].nodeValue);
-	document.write("<span class=\"arrow\"></span></a>");
-	var body = list[i].getElementsByTagName("body");
-	document.write("<ul>");
-	for (var j=0;j<body.length;j++){
-		document.write("<li><a href=\"");
-		l = body[j].getElementsByTagName("link")[0];
-		n = body[j].getElementsByTagName("name")[0];
-		document.write(l.childNodes[0].nodeValue);
-		document.write("\">");
-		document.write(n.childNodes[0].nodeValue);
-		document.write("</a></li>");
-	}
-	document.write("</ul></li>");
+document.write("<div id='navigation'>");
+document.write("<div id='btn'>");
+document.write("<div class='line'></div>");
+document.write("<div class='line'></div>");
+document.write("<div class='line'></div>");
+document.write("<div class='text'>MENU</div>");
+document.write("</div>");
+document.write("<div id='list'>");
+document.write("<ul>");
+var naviList = xmlDoc.getElementsByTagName("navigation")[0].getElementsByTagName('name');
+for (var i=0;i<naviList.length;i++){
+	document.write("<li><a href='");
+	document.write(xmlDoc.getElementsByTagName("navigation")[0].getElementsByTagName('link')[i].childNodes[0].nodeValue);
+	document.write("'>");
+	document.write(naviList[i].childNodes[0].nodeValue);
+	document.write("</a></li>");
 }
-var right = xmlDoc.getElementsByTagName("navigation")[0].getElementsByTagName("right")[0];
-document.write("<li class=\"right a\"><a href=\"");
-document.write(right.getElementsByTagName("link")[0].childNodes[0].nodeValue);
-document.write("\" style=\"width:30px;\"><img src=\"");
-document.write(right.getElementsByTagName("src")[0].childNodes[0].nodeValue);
-document.write("\" height=\"30px\"></a></li>");
 document.write("</ul>");
 document.write("</div>");
+document.write("</div>");
+$(document).ready(function(){
+	$("#body").css({
+		position: "absolute",
+		width: window.innerWidth,
+		height: window.innerHeight,
+	});
+	var ns = 0;
+	$("#navigation #btn").click(function(){
+		if (ns == 0){
+			$("#body").animate({left:300},300).addClass("box-shadow");
+			$("#navigation").animate({width:300,height:window.innerHeight},300);
+			$("#navigation .text").css({display:"block"});
+			$("#navigation #list").css({display:"block"});
+			ns = 1;
+		}else{
+			$("#navigation").animate({width:50,height:50},300);
+			$("#body").removeClass("box-shadow").animate({left:0},300);
+			$("#navigation .text").css({display:"none"});
+			$("#navigation #list").css({display:"none"});
+			ns = 0;
+		}
+	});
+	$("#body").click(function(){
+		if (ns == 1){
+			$("#navigation").animate({width:50,height:50},300);
+			$("#body").removeClass("box-shadow").animate({left:0},300);
+			$("#navigation .text").css({display:"none"});
+			$("#navigation #list").css({display:"none"});
+			ns = 0;
+		}
+	});
+});
