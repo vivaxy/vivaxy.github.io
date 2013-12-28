b7c6a67b8e5357412759c29072bf46d3d8249123
vivaxy <xyxuye2007@126.com>
2013-12-28 19:03:21 +0800

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -22,40 +22,47 @@ for (var i=0;i<naviList.length;i++){
 document.write("</ul>");
 document.write("</div>");
 document.write("</div>");
-$(document).ready(function(){
+
+$(document).ready(function () {
+	$("#navigation").css({
+		width : 300,
+		height : window.innerHeight,
+	});
 	$("#body").css({
-		position: "absolute",
-		width: window.innerWidth,
-		height: window.innerHeight,
+		position : "absolute",
+		width : window.innerWidth,
+		height : window.innerHeight,
 	});
 	var ns = 0;
-	$("#navigation #btn").click(function(){
-		if (ns == 0){
-			$("#body").animate({left:300},300).addClass("box-shadow");
-			$("#navigation").animate({width:300,height:window.innerHeight},300);
-			$("#navigation #list").css({display:"block"}).animate({left:0},600);
-			$("#navigation .text").css({display:"block"});
-			ns = 1;
-		}else{
-			$("#navigation #list").animate({left:-300},300,function(){
-				$(this).css({display:"none"});
+	$("#navigation #btn").click(function () {
+		if (ns == 0) {
+			$("#body").animate({left : 300}, 300, function () {
+				$(this).addClass("box-shadow");
+				$("#navigation #list").css({display : "block"}).animate({left : 0}, 300, function () {
+					$("#navigation .text").css({display : "block"});
+					ns = 1;
+				});
 			});
-			$("#navigation").animate({width:50,height:50},600);
-			$("#body").removeClass("box-shadow").animate({left:0},600);
-			$("#navigation .text").css({display:"none"});
-			ns = 0;
+		} else {
+			$("#navigation .text").css({display : "none"});
+			$("#navigation #list").animate({left : -300}, 300, function () {
+				$(this).css({display : "none"});
+				$("#body").removeClass("box-shadow").animate({left : 0}, 300, function () {
+					ns = 0;
+				});
+			});
 		}
 	});
-	$("#body").click(function(){
-		if (ns == 1){
-			$("#navigation #list").animate({left:-300},300,function(){
-				$(this).css({display:"none"});
+	$("#body").click(function () {
+		if (ns == 1) {
+			$("#navigation .text").css({display : "none"});
+			$("#navigation #list").animate({left : -300}, 300, function () {
+				$(this).css({display : "none"});
+				$("#body").removeClass("box-shadow").animate({left : 0}, 300, function () {
+					ns = 0;
+				});
 			});
-			$("#navigation").animate({width:50,height:50},600);
-			$("#body").removeClass("box-shadow").animate({left:0},600);
-			$("#navigation .text").css({display:"none"});
-			ns = 0;
 		}
 	});
-	
+
 });
