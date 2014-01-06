fa6830f5583e076bcee4d487409182023cf94421
vivaxy <xyxuye2007@126.com>
2014-01-06 11:12:37 +0800

    improve book

--- p/book/book.js
+++ p/book/book.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 	$.fn.fullpage({
 		slidesColor: ['#f2f2f2','#f2f2f2','#f2f2f2','#f2f2f2','#f2f2f2','#f2f2f2','#f2f2f2','#f2f2f2','#f2f2f2','#f2f2f2','#f2f2f2','#f2f2f2','#f2f2f2','#f2f2f2','#f2f2f2','#f2f2f2','#f2f2f2','#f2f2f2','#f2f2f2'],
-		anchors: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19']
+		anchors: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19'],
+		menu: '#menu',
 	});
 	$("img").each(function(){
-		if (this.height > window.innerHeight){
+		if (this.height/this.width > window.innerHeight/window.innerWidth){
 			this.height = window.innerHeight;
-		}
-		if (this.width > window.innerWidth){
+		}else{
 			this.width = window.innerWidth;
 		}
 	})
