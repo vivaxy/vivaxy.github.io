e37f27f1f3d244fda0675be42de9b9d629709d6a
vivaxy <xyxuye2007@126.com>
2014-01-06 15:16:16 +0800

    improve navi

--- js/navigation.js
+++ js/navigation.js
@@ -1,8 +1,8 @@
 var xmlhttp=new XMLHttpRequest();
-xmlhttp.open("GET","/conf/navigation.xml",false);
+xmlhttp.open("GET","/conf/navi.xml",false);
 xmlhttp.send();
 var xmlDoc=xmlhttp.responseXML;
-document.write("<div id='navigation'>");
+document.write("<div id='navi'>");
 document.write("<div class='btn'>");
 document.write("<div class='line'></div>");
 document.write("<div class='line'></div>");
@@ -11,10 +11,10 @@ document.write("<div class='text'>MENU</div>");
 document.write("</div>");
 document.write("<div class='list'>");
 document.write("<ul>");
-var naviList = xmlDoc.getElementsByTagName("navigation")[0].getElementsByTagName("name");
+var naviList = xmlDoc.getElementsByTagName("navi")[0].getElementsByTagName("name");
 for (var i=0;i<naviList.length;i++){
 	document.write("<li><a href='");
-	document.write(xmlDoc.getElementsByTagName("navigation")[0].getElementsByTagName("link")[i].childNodes[0].nodeValue);
+	document.write(xmlDoc.getElementsByTagName("navi")[0].getElementsByTagName("link")[i].childNodes[0].nodeValue);
 	document.write("'>");
 	document.write(naviList[i].childNodes[0].nodeValue);
 	document.write("</a></li>");
@@ -24,21 +24,21 @@ document.write("</div>");
 document.write("</div>");
 
 $(document).ready(function () {
-	$("#navigation .list").css({
+	$("#navi .list").css({
 		height : window.innerHeight - 50,
 	});
 	var ns = 0;
-	$("#navigation").click(function () {
+	$("#navi").click(function () {
 		if (ns == 0) {
-			$("#navigation").css({
+			$("#navi").css({
 				width : 300,
 				height : "100%",
 			});
 			$("#body").css({zIndex:100}).animate({left : 300}, 300, function () {
-				$("#navigation .list").css({display:"block"});
-				$("#navigation .text").css({display:"block"});
+				$("#navi .list").css({display:"block"});
+				$("#navi .text").css({display:"block"});
 				var listC = 0;
-				$("#navigation .list ul li").each(function(){
+				$("#navi .list ul li").each(function(){
 					$(this).delay(listC*40).animate({left : 0},100);
 					listC = listC + 1;
 					$("#body").css({zIndex:0});
@@ -48,10 +48,10 @@ $(document).ready(function () {
 		} else {
 			$("#body").css({zIndex:100}).animate({left : 0}, 300, function () {
 				ns = 0;
-				$("#navigation .text").css({display:"none"});
-				$("#navigation .list").css({display:"none"});
-				$("#navigation .list ul li").css({left : -300});
-				$("#navigation").css({width : 50,height : 50});
+				$("#navi .text").css({display:"none"});
+				$("#navi .list").css({display:"none"});
+				$("#navi .list ul li").css({left : -300});
+				$("#navi").css({width : 50,height : 50});
 				$(this).css({zIndex:0});
 			});
 		}
@@ -60,10 +60,10 @@ $(document).ready(function () {
 		if (ns == 1) {
 			$("#body").css({zIndex:100}).animate({left : 0}, 300, function () {
 				ns = 0;
-				$("#navigation .text").css({display:"none"});
-				$("#navigation .list").css({display:"none"});
-				$("#navigation .list ul li").css({left : -300});
-				$("#navigation").css({width : 50,height : 50});
+				$("#navi .text").css({display:"none"});
+				$("#navi .list").css({display:"none"});
+				$("#navi .list ul li").css({left : -300});
+				$("#navi").css({width : 50,height : 50});
 				$(this).css({zIndex:0});
 			});
 		}
