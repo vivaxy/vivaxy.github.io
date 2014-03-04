13f193fec841f87aebb5f31820aae1c3645fe336
vivaxy <xyxuye2007@126.com>
2014-03-04 08:38:41 +0800

    add 0;

---
+++ 0/main.js
@@ -0,0 +1,13 @@
+var text = "that's for u";
+
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var letterColors = [red, orange, green, blue, purple];
+
+drawName(text, letterColors);
+bubbleShape = 'circle';
+//bounceName();
+bounceBubbles();
