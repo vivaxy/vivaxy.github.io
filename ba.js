cdcc3bd45c741b7c11147a0d526026adf7c1bdbb
vivaxy <xyxuye2007@126.com>
2014-05-11 18:39:20 +0800

    add baidu tongji;

---
+++ ba.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?1907e318e2efbb82d9cd8bb2265f715d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
