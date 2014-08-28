af96a8c6df51cfe301603c49d3044a83e831f1cd
vivaxy <xyxuye2007@126.com>
2014-08-28 16:45:11 +0800

    improve menu.xml;

--- menu.js
+++ menu.js
@@ -1,15 +1,15 @@
-var xmlhttp=new XMLHttpRequest();
-xmlhttp.open("GET","menu.xml",false);
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", "menu.xml", false);
 xmlhttp.send();
-var xmlDoc=xmlhttp.responseXML;
+var xmlDoc = xmlhttp.responseXML;
 var menu = "";
-var menuList = xmlDoc.getElementsByTagName("menu")[0].getElementsByTagName("name");
-for (var i=0;i<menuList.length;i++){
-	menu = menu +
-		"<div><a target='_blank' href='" +
-		xmlDoc.getElementsByTagName("menu")[0].getElementsByTagName("link")[i].childNodes[0].nodeValue +
-		"'>" +
-		menuList[i].childNodes[0].nodeValue +
-		"</a></div>";
+var menuItemList = xmlDoc.getElementsByTagName("menu")[0].getElementsByTagName("item");
+for (var i = 0; i < menuItemList.length; i++) {
+    menu = menu +
+        "<div><a target='_blank' href='" +
+        menuItemList[i].getElementsByTagName("link")[0].childNodes[0].nodeValue +
+        "'>" +
+        menuItemList[i].getElementsByTagName("name")[0].childNodes[0].nodeValue +
+        "</a></div>";
 }
 document.write(menu);
