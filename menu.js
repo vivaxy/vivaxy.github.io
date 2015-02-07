74c9389f4b8577a2ffdabc3bd1c4b6ba7fcb3384
vivaxy <xyxuye2007@126.com>
2015-02-07 17:19:19 +0800

    change style

--- menu.js
+++ menu.js
@@ -10,10 +10,10 @@ var menu = "";
 var menuItemList = xmlDoc.getElementsByTagName("menu")[0].getElementsByTagName("item");
 for (var i = 0; i < menuItemList.length; i++) {
     menu = menu +
-        "<div><a target='_blank' href='" +
-        menuItemList[i].getElementsByTagName("link")[0].childNodes[0].nodeValue +
-        "'>" +
-        menuItemList[i].getElementsByTagName("name")[0].childNodes[0].nodeValue +
-        "</a></div>";
+    "<div><a target='_blank' href='" +
+    menuItemList[i].getElementsByTagName("link")[0].childNodes[0].nodeValue +
+    "'>" +
+    menuItemList[i].getElementsByTagName("name")[0].childNodes[0].nodeValue +
+    "</a></div>";
 }
 document.write(menu);
