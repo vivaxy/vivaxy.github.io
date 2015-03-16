1b1e82fa12105ba792167be7458f2856896d184a
vivaxy <xyxuye2007@126.com>
2015-03-16 10:40:20 +0800

    dp

--- dp/index.js
+++ dp/index.js
@@ -10,23 +10,24 @@ var textarea = document.querySelector('textarea'),
 
         var a = document.createElement('a');
         a.href = url;
-        var result = a.search.match(new RegExp('[?&]' + name + '=([^&]+)', 'i'));
-        if (result == null || result.length < 1) {
+        var search = a.search || a.hash;
+        var result = search.match(new RegExp('[?&]' + name + '=([^&]+)', 'i'));
+        if (result === null || result.length < 1) {
             return '';
         }
         return result[1];
 
-    },
-
-    text = textarea.value,
-    uuid = getQueryStringByName(text, 'uuid_token'),
-    signLink = 'http://qywx.dper.com/app/checkin.html#/sign?uuid_token=' + uuid + '&longitude=121.41546&latitude=31.217155',
-    checkLink = 'http://qywx.dper.com/app/checkin.html#/search?uuid_token=' + uuid;
+    };
 
 sign.addEventListener('click', function () {
-    location.href = signLink;
+    var text = textarea.value,
+        uuid = getQueryStringByName(text, 'uuid_token');
+    location.href = 'http://qywx.dper.com/app/checkin.html#/sign?uuid_token=' + uuid + '&longitude=121.41546&latitude=31.217155';
 });
 
 check.addEventListener('click', function () {
-    location.href = checkLink;
+    var text = textarea.value,
+        uuid = getQueryStringByName(text, 'uuid_token');
+    location.href = 'http://qywx.dper.com/app/checkin.html#/search?uuid_token=' + uuid;
+
 });
