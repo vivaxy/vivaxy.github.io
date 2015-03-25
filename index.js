c2605e3a465b7efba185985a3ece465353d4ab68
vivaxy <xyxuye2007@126.com>
2015-03-25 20:38:52 +0800

    style

--- index.js
+++ index.js
@@ -15,10 +15,12 @@ var menuList = JSON.parse(json).menu;
 var frag = document.createDocumentFragment();
 for (var i = 0; i < menuList.length; i++) {
     var menu = menuList[i];
-    var a = document.createElement('a');
+    var a = document.createElement('a'),
+        span = document.createElement('span');
     a.target = '_blank';
     a.href = menu.link;
-    a.innerText = menu.name;
+    span.innerText = menu.name;
+    a.appendChild(span);
     frag.appendChild(a);
 }
 document.body.appendChild(frag);
