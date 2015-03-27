28f0a5c3d3ff95039f44b645e37bc17016f8ea42
vivaxy <xyxuye2007@126.com>
2015-03-27 21:00:23 +0800

    resume

--- resume/animation.js
+++ resume/animation.js
@@ -1,17 +1,10 @@
-var cards = Array.prototype.slice.call(document.querySelectorAll(".card"));
-function getQueryStringByName(name){
-	var result = location.search.match(new RegExp("[\?\&]" + name+ "=([^\&]+)","i"));
-	if(result == null || result.length < 1){
-		return "";
-	}
-	return result[1];
-}
-var style = getQueryStringByName("style");
-if (style == "") style = "opacity";
-var show = function(i){
-	cards[i].classList.remove(style);
-};
-for (var i in cards){
-	cards[i].classList.add(style);
-	setTimeout(show, i*200, i);
-}
+var url = new Url(),
+    style = url.parameter('style') || 'opacity';
+
+Array.prototype.slice.call(document.querySelectorAll(".card")).forEach(function (card, index) {
+    card.classList.remove('hide');
+    card.classList.add(style);
+    setTimeout(function () {
+        card.classList.remove(style);
+    }, (index + 1) * 200);
+});
