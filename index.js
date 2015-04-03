fc7733001e3937cefea4339697fbcf30050d6c6e
vivaxy <xyxuye2007@126.com>
2015-04-03 15:50:46 +0800

    ajax git ignore

--- index.js
+++ index.js
@@ -4,23 +4,26 @@
  */
 // send ajax
 var req = new XMLHttpRequest();
-req.open("GET", "menu.json", false);
+req.open("GET", "menu.json", true);
+req.addEventListener('readystatechange', function () {
+    if (req.readyState === 4 && req.status === 200) {
+        // get data
+        var json = req.responseText;
+        var menuList = JSON.parse(json);
+
+        // append to html
+        var frag = document.createDocumentFragment();
+        for (var i = 0; i < menuList.length; i++) {
+            var menu = menuList[i];
+            var a = document.createElement('a'),
+                span = document.createElement('span');
+            a.target = '_blank';
+            a.href = menu.link;
+            span.innerText = menu.name;
+            a.appendChild(span);
+            frag.appendChild(a);
+        }
+        document.body.appendChild(frag);
+    }
+});
 req.send();
-
-// get data
-var json = req.responseText;
-var menuList = JSON.parse(json).menu;
-
-// append to html
-var frag = document.createDocumentFragment();
-for (var i = 0; i < menuList.length; i++) {
-    var menu = menuList[i];
-    var a = document.createElement('a'),
-        span = document.createElement('span');
-    a.target = '_blank';
-    a.href = menu.link;
-    span.innerText = menu.name;
-    a.appendChild(span);
-    frag.appendChild(a);
-}
-document.body.appendChild(frag);
