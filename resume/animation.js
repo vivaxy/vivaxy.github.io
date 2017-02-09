1d7257787aacaa7f0ff8077e4173dcf9feb98c28
vivaxy <xyxuye2007@126.com>
2017-02-09 18:42:30 +0800

    feat(resume): :sparkles:

--- resume/animation.js
+++ resume/animation.js
@@ -1,10 +1,10 @@
-var url = new Url(),
-    style = url.parameter('style') || 'opacity';
+var url = new Url();
+var style = url.parameter('style') || 'opacity';
 
-Array.prototype.slice.call(document.querySelectorAll(".card")).forEach(function (card, index) {
+Array.prototype.slice.call(document.querySelectorAll('.card')).forEach(function(card, index) {
     card.classList.remove('hide');
     card.classList.add(style);
-    setTimeout(function () {
+    setTimeout(function() {
         card.classList.remove(style);
     }, (index + 1) * 200);
 });
