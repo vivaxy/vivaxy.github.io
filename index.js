81a04a372899c787b148ab32a4aea96e37756f93
vivaxy <xyxuye2007@126.com>
2018-05-30 10:40:11 +0800

    chore: :art:

--- index.js
+++ index.js
@@ -6,14 +6,14 @@
 var req = new XMLHttpRequest();
 req.open('GET', 'menu.json');
 req.addEventListener('readystatechange', function() {
-    if (req.readyState === 4 && req.status === 200) {
-        JSON.parse(req.responseText).forEach(function(menu) {
-            var a = document.createElement('a');
-            a.target = '_blank';
-            a.href = menu.link;
-            a.textContent = menu.name;
-            document.body.appendChild(a);
-        });
-    }
+  if (req.readyState === 4 && req.status === 200) {
+    JSON.parse(req.responseText).forEach(function(menu) {
+      var a = document.createElement('a');
+      a.target = '_blank';
+      a.href = menu.link;
+      a.textContent = menu.name;
+      document.body.appendChild(a);
+    });
+  }
 });
 req.send();
--- resume/animation.js
+++ resume/animation.js
@@ -3,9 +3,9 @@ var urlSearchParams = url.searchParams;
 var style = urlSearchParams.get('style') || 'opacity';
 
 Array.prototype.slice.call(document.querySelectorAll('.card')).forEach(function(card, index) {
-    card.classList.remove('hide');
-    card.classList.add(style);
-    setTimeout(function() {
-        card.classList.remove(style);
-    }, (index + 1) * 200);
+  card.classList.remove('hide');
+  card.classList.add(style);
+  setTimeout(function() {
+    card.classList.remove(style);
+  }, (index + 1) * 200);
 });
