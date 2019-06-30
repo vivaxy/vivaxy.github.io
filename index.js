c574030d52483ea27d487c9f042171fa336b6d91
vivaxy <xyxuye2007@126.com>
2019-06-30 13:33:05 +0800

    feat(link): :sparkles: Remove open in page

--- index.js
+++ index.js
@@ -2,14 +2,12 @@
  * @since 2014/9/9 9:11
  * @author vivaxy
  */
-
-var req = new XMLHttpRequest();
+const req = new XMLHttpRequest();
 req.open('GET', 'menu.json');
 req.addEventListener('readystatechange', function() {
   if (req.readyState === 4 && req.status === 200) {
     JSON.parse(req.responseText).forEach(function(menu) {
-      var a = document.createElement('a');
-      a.target = '_blank';
+      const a = document.createElement('a');
       a.href = menu.link;
       a.textContent = menu.name;
       document.body.appendChild(a);
