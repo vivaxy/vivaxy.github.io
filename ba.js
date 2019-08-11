02c1bd21941bb86d07c957389ec5a02e30aa5d08
vivaxy <xyxuye2007@126.com>
2019-08-11 13:51:32 +0800

    chore(ba): :chart_with_upwards_trend:

--- ba.js
+++ ba.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement('script');
-  hm.src = '//hm.baidu.com/hm.js?55a71d6aca6ade58d30193da2d07e6f8';
+  hm.src = '//hm.baidu.com/hm.js?ed1e5255581c4562779f098ceaef67ee';
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(hm, s);
 })();
