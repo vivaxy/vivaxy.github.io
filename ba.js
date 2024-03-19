4e3e3e1d633ae9de221c21fc8c14530c3327eaad
vivaxy <xyxuye2007@126.com>
2024-03-19 17:17:43 +0800

    feat(gtag): add gtag

--- ba.js
+++ ba.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement('script');
-  hm.src = '//hm.baidu.com/hm.js?ed1e5255581c4562779f098ceaef67ee';
+  hm.src = 'https://hm.baidu.com/hm.js?2033bd30a5f5c932870e20fff5bc28ce';
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(hm, s);
 })();
---
+++ gtag.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-2Y9QX14PS6');
+const s = document.createElement('script');
+s.src = 'https://www.googletagmanager.com/gtag/js?id=G-2Y9QX14PS6';
+document.body.appendChild(s);
