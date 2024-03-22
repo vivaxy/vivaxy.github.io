d1fe94c12b01e28dfa672cf5c491e0c5d9ad45bd
vivaxy <xyxuye2007@126.com>
2024-03-22 10:30:42 +0800

    feat(common): merge ba, gtag, mclarity into common.js

--- ba.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement('script');
-  hm.src = 'https://hm.baidu.com/hm.js?ed1e5255581c4562779f098ceaef67ee';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(hm, s);
-})();
---
+++ cm.js
@@ -0,0 +1,11 @@
+/**
+ * @since 2024-03-22
+ * @author vivaxy
+ * cm => common
+ */
+// ba.js
+var _hmt = _hmt || [];(function() {var hm = document.createElement('script');hm.src = 'https://hm.baidu.com/hm.js?ed1e5255581c4562779f098ceaef67ee';var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(hm, s);})();
+// gtag.js
+window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-2Y9QX14PS6');var s = document.createElement('script');s.src = 'https://www.googletagmanager.com/gtag/js?id=G-2Y9QX14PS6';document.body.appendChild(s);
+// mclarity.js
+(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src='https://www.clarity.ms/tag/'+i+'?ref=bwt';y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, 'clarity', 'script', 'ljh5t3oxf2');
