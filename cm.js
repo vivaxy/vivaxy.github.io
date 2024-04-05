7d0159c8f761c92db0c63e7d6feb84ca69042c2a
vivaxy <xyxuye2007@126.com>
2024-04-05 20:20:34 +0800

    feat(common): add google adsense

--- cm.js
+++ cm.js
@@ -4,8 +4,41 @@
  * cm => common
  */
 // ba.js
-var _hmt = _hmt || [];(function() {var hm = document.createElement('script');hm.src = 'https://hm.baidu.com/hm.js?ed1e5255581c4562779f098ceaef67ee';var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(hm, s);})();
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement('script');
+  hm.src = 'https://hm.baidu.com/hm.js?ed1e5255581c4562779f098ceaef67ee';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(hm, s);
+})();
 // gtag.js
-window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-2Y9QX14PS6');var s = document.createElement('script');s.src = 'https://www.googletagmanager.com/gtag/js?id=G-2Y9QX14PS6';document.body.appendChild(s);
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-2Y9QX14PS6');
+var s = document.createElement('script');
+s.src = 'https://www.googletagmanager.com/gtag/js?id=G-2Y9QX14PS6';
+document.body.appendChild(s);
 // mclarity.js
-(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src='https://www.clarity.ms/tag/'+i+'?ref=bwt';y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, 'clarity', 'script', 'ljh5t3oxf2');
+(function (c, l, a, r, i, t, y) {
+  c[a] =
+    c[a] ||
+    function () {
+      (c[a].q = c[a].q || []).push(arguments);
+    };
+  t = l.createElement(r);
+  t.async = 1;
+  t.src = 'https://www.clarity.ms/tag/' + i + '?ref=bwt';
+  y = l.getElementsByTagName(r)[0];
+  y.parentNode.insertBefore(t, y);
+})(window, document, 'clarity', 'script', 'ljh5t3oxf2');
+// adsense
+(function () {
+  var s = document.createElement('script');
+  s.src =
+    'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9427645733264547';
+  s.crossOrigin = 'anonymous';
+  document.head.appendChild(s);
+})();
